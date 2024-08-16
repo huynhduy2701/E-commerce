d4f88547574af1e4db77637e7a118c6fc05963dd
huynhduy <nlhd.2701@gmail.com>
2024-08-16 17:12:28 +0700

    afternoon 16/08/2024

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+// === SWIPER CATEGORY ===
+var swiperCategories = new Swiper(".categories_container", {
+    spaceBetween: 10,
+    loop :true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1200: {
+          slidesPerView: 6,
+          spaceBetween: 24,
+        },
+      },
+  });
