e25201c8e82c8c637810d091670c5675659710cf
huynhduy <nlhd.2701@gmail.com>
2024-08-18 17:58:36 +0700

    5:58 18/8/2024

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,3 +42,29 @@ var swiperCategories = new Swiper(".categories_container", {
       tab.classList.add("active-tab");
     })
   })
+
+
+  // === NEW ARRIVALS ===
+  var swiperProducts= new Swiper(".new_container", {
+    spaceBetween: 10,
+    loop :true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 24,
+        },
+      },
+  });
